057a075ec42389726e48096c3586bbd2e13532dc
Lisiasty <sonicmephit@gmail.com>
2024-04-07 23:34:55 +0200

    Added exciseTax variable

--- src/main/java/pl/edu/agh/mwo/invoice/product/BottleOfWine.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/BottleOfWine.java
@@ -3,6 +3,8 @@ package pl.edu.agh.mwo.invoice.product;
 import java.math.BigDecimal;
 
 public class BottleOfWine extends Product {
+
+    BigDecimal exciseTax = BigDecimal.valueOf(5.56);
     public BottleOfWine(String name, BigDecimal price) {
         super(name, price, new BigDecimal("0.23"));
     }
--- src/main/java/pl/edu/agh/mwo/invoice/product/FuelCanister.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/FuelCanister.java
@@ -3,6 +3,7 @@ package pl.edu.agh.mwo.invoice.product;
 import java.math.BigDecimal;
 
 public class FuelCanister extends Product {
+    BigDecimal exciseTax = BigDecimal.valueOf(5.56);
     public FuelCanister(String name, BigDecimal price) {
         super(name, price, new BigDecimal("0"));
     }
